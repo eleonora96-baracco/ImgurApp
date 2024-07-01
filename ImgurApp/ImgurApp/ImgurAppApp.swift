b7aba97f875e639805734fec9d45d0d1ba6e3998
Eleonora <eleonora.baracco@virgilio.it>
2024-07-01 08:11:50 +0200

    project creation

---
+++ ImgurApp/ImgurApp/ImgurAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImgurAppApp.swift
+//  ImgurApp
+//
+//  Created by Michele Apicella on 01/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ImgurAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
